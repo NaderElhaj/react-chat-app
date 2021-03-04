652e780d5cbc0e28b37fa8a349cbcaee321c8a20
Nader elhaj <naderelhaj3@gmail.com>
2021-03-04 07:10:29 +0100

    Structuring the Header

---
+++ src/Header.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+import React from 'react'
+import './Header.css'
+import { Avatar } from '@material-ui/core'
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search'
+import HelpOutLineIcon from '@material-ui/icons/HelpOutline'
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+               <Avatar 
+                   className="header__avatar"
+                //    alt={user?.displayName}
+                //    src={user?.photoURL}
+               /> 
+               <AccessTimeIcon />
+            </div>
+            <div className="header__search">
+                <SearchIcon />
+                <input placeholder="Search "/>
+            </div>
+            <div className="header_right">
+                <HelpOutLineIcon />
+            </div>
+        </div>
+    )
+}
+
+export default Header
