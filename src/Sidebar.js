846c86926c7872ca00eda9c125beb45491b4e74c
Nader elhaj <naderelhaj3@gmail.com>
2021-03-05 10:30:02 +0100

    Structuring Sidebar Header

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Sidebar.css'
-
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
+import CreateIcon from '@material-ui/icons/Create'
 function Sidebar() {
     return (
         <div className="sidebar">
-            
+           <div className="sidebar__header">
+               <div className="sidebar__info">
+                <h2>Nader Elhaj</h2>
+                <h3>
+                    <FiberManualRecordIcon />
+                    Nader Elhaj
+                </h3>
+               </div>
+               <CreateIcon />
+           </div> 
         </div>
     )
 }
